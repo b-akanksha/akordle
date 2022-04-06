4852020d3dd482448215584a55253013fd01bca3
Akanksha <b.akanksha6@gmail.com>
2022-04-06 09:26:13 +0530

    react-18 create root use

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import * as ReactDOMClient from 'react-dom/client'
 
 import { Provider } from 'react-redux'
 import store from './store'
@@ -8,13 +8,16 @@ import './index.scss'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
-ReactDOM.render(
+const container = document.getElementById('root')
+
+const root = ReactDOMClient.createRoot(container)
+
+root.render(
     <React.StrictMode>
         <Provider store={store}>
             <App />
         </Provider>
-    </React.StrictMode>,
-    document.getElementById('root')
+    </React.StrictMode>
 )
 
 // If you want to start measuring performance in your app, pass a function
