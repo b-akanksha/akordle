77694e21f768a13593b97f59259fd79e5203ab2c
Akanksha <b.akanksha6@gmail.com>
2022-04-06 12:14:43 +0530

    store config

--- src/store.js
+++ src/store.js
@@ -1,12 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 
 import akordleReducer from './redux/akordle'
-
+console.log('process.env.NODE_ENV ', process.env.NODE_ENV)
 const store = configureStore({
     reducer: {
         akordle: akordleReducer,
     },
-    devTools: process.env.NODE_ENV === 'development',
+    devTools: false,
 })
 
 export default store
