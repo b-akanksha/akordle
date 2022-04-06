1da5e113ab373e89ba93e760895168b5930e2006
Akanksha <b.akanksha6@gmail.com>
2022-04-06 11:41:03 +0530

    prompt integration

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from './components/header/Header'
 import './App.scss'
 import AkordleInfo from './components/akordleInfo/AkordleInfo'
 import Board from './components/boad/Board'
 import Keyboard from './components/keyboard/Keyboard'
+import Prompt from './components/prompt/Prompt'
+import { useDispatch } from 'react-redux'
+import { resetGame } from './redux/akordle'
 
 function App() {
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(resetGame())
+    }, [dispatch])
+
     return (
         <main>
             <div className="center-content">
@@ -13,6 +22,7 @@ function App() {
                 <AkordleInfo />
                 <Board />
                 <Keyboard />
+                <Prompt />
             </div>
         </main>
     )
